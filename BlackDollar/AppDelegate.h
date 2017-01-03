6dcd32e5ce37fb54ca5c88a0b17b5ee8a9589cad
Eugene Zhuk <eugene.zhuk.dev@gmail.com>
2017-01-03 18:45:54 +0200

    initial

---
+++ BlackDollar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlackDollar
+//
+//  Created by Eugene Zhuk Home on 1/3/17.
+//  Copyright © 2017 Eugene Zhuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlackDollar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlackDollar
+//
+//  Created by Eugene Zhuk Home on 1/3/17.
+//  Copyright © 2017 Eugene Zhuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
